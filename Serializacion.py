a3bb22a2f94466e044a6843458988633e9dcae07
xXZaulXx <zaulhdz.@gmail.com>
2023-10-12 18:49:32 -0600

    Creamos una lista de docentes con el objeto creado anteriormente

--- Serializacion.py
+++ Serializacion.py
@@ -10,3 +10,16 @@ def crear_docente(nombre, numero_empleado, especialidad):
         "especialidad": especialidad
     }
 
+# Creamos una lista de docentes
+docentes = [
+    crear_docente("Roberto Perez", 10023, "Haking etico"),
+    crear_docente("Maria Azucena", 10024, "Direccion de proyectos 2"),
+    crear_docente("Patricia Guadalupe", 10025, "Inteligencia de negocios"),
+    crear_docente("Gabriel Alejandro", 10026, "Automatizacion de infraestructura digital"),
+    crear_docente("Alan Dias", 10027, "sistemas de calidad"),
+    crear_docente("Geltrudis pilar", 10028, "Ingles"),
+    crear_docente("Roberto Perez", 10029, "Tutorias"),
+    crear_docente("Adriana", 10030, "Equipos de alto rendimiento"),
+    crear_docente("Zaul Hernandez", 19040067, "Alumno"),
+]
+
